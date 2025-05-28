1c146ea41a376e07886e9921f7ae141f85753d42
Ysaias Alvarez <isaiasalvarez@gmail.com>
2025-05-28 18:16:40 -0400

    update url

--- demo-app/react-demo-app/src/oidc-config.js
+++ demo-app/react-demo-app/src/oidc-config.js
@@ -1,7 +1,7 @@
 export const oidcSettings = {
-  authority: 'http://localhost:8088/v1/esignet/oidc/',
+  authority: 'http://146.190.122.50:8088/v1/esignet/oidc/',
   client_id: 'IIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAoVh41',
-  redirect_uri: 'http://localhost:3001/callback',
+  redirect_uri: 'http://146.190.122.50:3001/callback',
   response_type: 'code',
   scope: 'openid',
   acrValues: 'mosip:idp:acr:generated-code',
